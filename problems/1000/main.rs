f10f9cf36f91ba68c5800488cfae2091b9e6b5e8
Felipe Almeida <142172332+lipenvs@users.noreply.github.com>
2026-02-10 12:09:46 -0300

    feat: solve problem 1000 in Rust

---
+++ problems/1000/main.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+    // Escreva a sua solução aqui
+// Escreva a sua solução aqui
+    // Code your solution here
+// Code your solution here
+    // Escriba su solución aquí
+// Escriba su solución aquí
+    println!("Hello World!");
+    
+}
